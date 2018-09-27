821cbafc9106574f947bc15abef78acc7a16c44d
Maxwell-Icharia <maxwellicharia@gmail.com>
2018-09-28 01:44:26 +0300

    [Edit] - Heroku CLI intergration

---
+++ poptraq/models.py
@@ -0,0 +1,28 @@
+from Poptraq import db
+
+
+class User(db.Model):
+    """ Create user table"""
+
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    national_id = db.Column(db.Integer)
+    first_name = db.Column(db.String(80))
+    surname = db.Column(db.String(80))
+    dob = db.Column(db.String(25))
+    home_county = db.Column(db.String(80))
+    email = db.Column(db.String(200), unique=True)
+    password = db.Column(db.String(80))
+
+    def __init__(self, national_id, first_name, surname, dob, home_county, email, password):
+        self.national_id = national_id
+        self.first_name = first_name
+        self.surname = surname
+        self.dob = dob
+        self.home_county = home_county
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
