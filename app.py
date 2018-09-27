5a57b955b2cf083ff36e58914edc8ebf0c07f521
Maxwell-Icharia <maxwellicharia@gmail.com>
2018-09-27 13:56:00 +0300

    [Edit] - Rename main file

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template, request, url_for
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, Manager
 from form import Signup
+from models import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "$poptraq#"
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://localhost/poptraq"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager = Manager(app)
 Bootstrap(app)
 
 
@@ -31,15 +35,23 @@ def signup():
     form = Signup(request.form)  # instantiating form to use the forms defined from the Form class in form.py
 
     if request.method == 'GET':
-        return render_template('create.html', form=form)
+        return render_template('signup.html', form=form)
     else:
         if not form.validate_on_submit():  # making sure that the form is validated before submission
-            return render_template('create.html', form=form, not_validate=True)
+            return render_template('signup.html', form=form, not_validate=True)
         else:
-            db = Models()
-            db.create()
-            name = request.form['note_name']
-            subject = request.form['note_subject']
-            content = request.form['note_content']
-            db.insert(name, subject, content)
-            return url_for('account', create=True)
+            user = User()
+            user.__init__(national_id=request.form['national_id'],
+                          first_name=request.form['first_name'],
+                          surname=request.form['surname'],
+                          dob=request.form['dob'],
+                          home_county=request.form['home_county'],
+                          email=request.form['email'],
+                          password=request.form['password'])
+            user.create()
+            return url_for('account')
+
+
+@app.route('/account/')
+def account():
+    return render_template("account.html")
