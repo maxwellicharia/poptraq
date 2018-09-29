e9046806bad6e5b3869a90695281a4d1c46e1bf4
Maxwell-Icharia <maxwellicharia@gmail.com>
2018-09-30 02:34:50 +0300

    [Create] - Blueprints' Views for routes

--- poptraq/user/views.py
+++ poptraq/user/views.py
@@ -1,8 +1,10 @@
-from flask import render_template, request, url_for, session, redirect
+from flask import render_template, request, url_for, session, redirect, Blueprint, abort
 from werkzeug.security import generate_password_hash, check_password_hash
 from poptraq.form import Signup, Login
 from poptraq.models import User
-from poptraq import user, db
+from poptraq import app, db
+
+user = Blueprint('user', __name__, url_prefix='/user', static_folder='static', template_folder='templates')
 
 
 @user.route('/signup', methods=['GET', 'POST'])
@@ -13,10 +15,9 @@ def signup():
         return render_template('signup.html', form=form)
     else:
         if not form.validate_on_submit():  # making sure that the form is validated before submission
-            return render_template('signup.html', form=form, not_validate=True)
+            return render_template('signup.html', form=form)
         else:
-            password = request.form['password']
-            pw_hash = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
+            pw_hash = generate_password_hash(request.form['password'], method='pbkdf2:sha256', salt_length=8)
             new_user = User(request.form['national_id'],
                             request.form['first_name'],
                             request.form['surname'],
@@ -26,8 +27,8 @@ def signup():
                             pw_hash)
             db.session.add(new_user)
             db.session.commit()
-            session['logged_in'] = True
-            return redirect(url_for('account'))
+            session['email'] = request.form['email']
+            return redirect(url_for('user.account'))
 
 
 @user.route('/login', methods=['GET', 'POST'])
@@ -38,26 +39,29 @@ def login():
         return render_template('login.html', form=form)
     else:
         if not form.validate_on_submit():  # making sure that the form is validated before submission
-            return render_template('login.html', form=form, not_validate=True)
+            return render_template('login.html', form=form)
         else:
             national_id = request.form['national_id']
             email = request.form['email'],
             password = request.form['password']
-            exists = User.query.filter_by(
-                    national_id=national_id).first()
+            exists = User.query.filter_by(national_id=national_id).first_or_404()
             if (email != exists.email) and (check_password_hash(exists.password, password) is False):
-                return redirect(url_for('login'))
+                abort(401)
             else:
-                return redirect(url_for('account'))
+                session['email'] = email
+                message = ("Successful login: ", email[0])
+                return render_template('account.html', message=message)
 
 
 @user.route('/logout', methods=['GET', 'POST'])
 def logout():
-    session['logged_in'] = False
-    return redirect(url_for('dash'))
+    session.pop('email', None)
+    return redirect(url_for('index'))
 
 
 @user.route('/account', methods=['GET', 'POST'])
-def account(id_no):
-    id_no = signup.national_id
-    return render_template('account.html', **locals())
+def account():
+    return render_template('account.html')
+
+
+app.register_blueprint(user)
