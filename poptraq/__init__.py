be7cc9cce2466c9713d7a7c7582c6b88c1d0fefe
Maxwell-Icharia <maxwellicharia@gmail.com>
2018-09-30 02:30:12 +0300

    [Create] - Package for imports

--- poptraq/__init__.py
+++ poptraq/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, Manager, MigrateCommand
 from flask_bootstrap import Bootstrap
+from flask_debug import Debug
 
 app = Flask(__name__)
 
@@ -11,9 +12,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 Bootstrap(app)
 
+Debug(app)
+
 db = SQLAlchemy(app)
 
-from poptraq.models import User
+from poptraq.models import User, County
+from poptraq import views
+from poptraq.user import views
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
