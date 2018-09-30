ef016bf5ffbeb2dbabde8aa0a9bdf9f6d7191424
Maxwell-Icharia <maxwellicharia@gmail.com>
2018-09-30 23:48:53 +0300

    [Edit] - Form fields

--- poptraq/form.py
+++ poptraq/form.py
@@ -1,36 +1,39 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, validators, IntegerField, PasswordField, FloatField
-# importing necessary fields and validators for the form
+from wtforms import StringField, IntegerField, PasswordField, FloatField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 
 class Signup(FlaskForm):
     """Template for signup form creation"""
 
-    national_id = IntegerField("National ID", [validators.DataRequired(message="Kindly input your National ID Number")])
-    first_name = StringField("First Name", [validators.DataRequired(message="Kindly your first name")])
-    surname = StringField("Surname", [validators.DataRequired(message="Kindly input your last name")])
-    dob = StringField("Date of Birth", [validators.DataRequired(message="Kindly input your Date of Birth")])
-    email = StringField("Email", [validators.DataRequired(message="Kindly input your Email Address")])
-    home_county = StringField("Home County", [validators.DataRequired(message="Kindly input your last name")])
-    password = PasswordField("Password", [validators.DataRequired(message="Input Password"), validators.Length(min=8),
-                                          validators.EqualTo('confirm_password', message='Passwords must match')])
+    national_id = IntegerField("National ID", [DataRequired(message="Kindly input your National ID Number")])
+    first_name = StringField("First Name", [DataRequired(message="Kindly your first name")])
+    surname = StringField("Surname", [DataRequired(message="Kindly input your last name")])
+    dob = StringField("Date of Birth", [DataRequired(message="Kindly input your Date of Birth")])
+    email = StringField("Email", [DataRequired(message="Kindly input your Email Address"), Email(
+        message="Invalid Email Address")])
+    home_county = StringField("Home County", [DataRequired(message="Kindly input your last name")])
+    password = PasswordField("Password", [DataRequired(message="Input Password"),
+                                          Length(min=8, message="Password too short > 8"),
+                                          EqualTo('confirm_password', message='Passwords must match')])
     confirm_password = PasswordField("Repeat Password")
 
 
 class Login(FlaskForm):
     """Template for Login Form creation"""
 
-    national_id = IntegerField("National ID", [validators.DataRequired(message="National ID required")])
-    email = StringField("Email", [validators.DataRequired(message="Email Address required")])
-    password = PasswordField("Password", [validators.DataRequired(message="Password required")])
+    national_id = IntegerField("National ID", [DataRequired(message="National ID required")])
+    email = StringField("Email", [DataRequired(message="Email Address required"), Email(
+        message="Invalid Email Address")])
+    password = PasswordField("Password", [DataRequired(message="Password required")])
 
 
 class County(FlaskForm):
     """County Template for data display"""
 
-    county_name = StringField("County Name", [validators.DataRequired(message="Name of county required")])
-    sub_counties = IntegerField("No. of Sub-counties", [validators.DataRequired(message="Required field")])
-    sectors = IntegerField("No. of sectors", [validators.DataRequired(message="Input County Government Sectors")])
-    population = FloatField("Population", [validators.DataRequired(message="Input estimated calculated population")])
-    budget = FloatField("Budget (Ksh)", [validators.DataRequired(message="Input allocated budget")])
-    size = FloatField("County Size", [validators.DataRequired(message="Input county size")])
+    county_name = StringField("County Name", [DataRequired(message="Name of county required")])
+    sub_counties = IntegerField("No. of Sub-counties", [DataRequired(message="Required field")])
+    sectors = IntegerField("No. of sectors", [DataRequired(message="Input County Government Sectors")])
+    population = FloatField("Population", [DataRequired(message="Input estimated calculated population")])
+    budget = FloatField("Budget (Ksh)", [DataRequired(message="Input allocated budget")])
+    size = FloatField("County Size", [DataRequired(message="Input county size")])
