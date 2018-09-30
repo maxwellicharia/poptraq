b9d4145d3ea0f281561850d8a44d7f744a7aea78
Maxwell-Icharia <maxwellicharia@gmail.com>
2018-09-30 23:52:11 +0300

    [Edit] - Routing and url-building - App entry point

--- poptraq/views.py
+++ poptraq/views.py
@@ -1,22 +1,33 @@
 import os
 import markdown
-from flask import render_template, send_from_directory, Markup
+from flask import render_template, send_from_directory, Markup, session
 from poptraq import app
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    if session.get('email') is None:
+        return render_template('index.html')
+    else:
+        email = session['email']
+        return render_template('index.html', email=email)
 
 
-@app.route('/about')
+@app.route('/about', methods=['GET'])
 def about():
-    with open("./docs/README.md") as f:
-        content = f.read()
-    content = Markup(markdown.markdown(content))
-    return render_template('about.html', **locals())
+    if session.get('email') is None:
+        with open("./docs/README.md") as f:
+            content = f.read()
+        content = Markup(markdown.markdown(content))
+        return render_template('about.html', content=content)
+    else:
+        email = session['email']
+        with open("./docs/README.md") as f:
+            content = f.read()
+        content = Markup(markdown.markdown(content))
+        return render_template('about.html', **locals())
 
 
 @app.route('/favicon.ico')
 def fav():
-    return send_from_directory(os.path.join(index.root_path, './static/img'), 'favicon.ico')
+    return send_from_directory(os.path.join(app.root_path, './static/img'), 'favicon.ico')
