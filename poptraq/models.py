2bfce937934be36ec13c868cb2a9da6c2ddc2f43
Maxwell-Icharia <maxwellicharia@gmail.com>
2018-10-08 00:15:53 +0300

    [Edit] - Database tables

--- poptraq/models.py
+++ poptraq/models.py
@@ -1,24 +1,31 @@
 from poptraq import db
 from datetime import datetime
+from flask_login import UserMixin
+from poptraq import login
 
 
-class User(db.Model):
+@login.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
     """ Create user table"""
 
     __tablename__ = 'user'
 
-    user_id = db.Column(db.Integer, nullable=False, primary_key=True)
-    national_id = db.Column(db.Integer, nullable=False,)
-    first_name = db.Column(db.String, nullable=False,)
-    surname = db.Column(db.String, nullable=False,)
-    dob = db.Column(db.Date, nullable=False,)
-    home_county = db.Column(db.String, nullable=False,)
+    national_id = db.Column(db.Integer, nullable=False, primary_key=True)
+    first_name = db.Column(db.String, nullable=False)
+    surname = db.Column(db.String, nullable=False)
+    dob = db.Column(db.Date, nullable=False)
+    home_county = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False, unique=True)
-    password = db.Column(db.String, nullable=False,)
+    password = db.Column(db.String, nullable=False)
     created = db.Column(db.TIMESTAMP, nullable=False, default=datetime.utcnow)
     last_seen = db.Column(db.TIMESTAMP, nullable=False, default=datetime.utcnow)
     confirmed = db.Column(db.Boolean, nullable=False, default=False)
     confirmed_on = db.Column(db.DateTime, nullable=True)
+    role = db.Column(db.String, nullable=False, default="User")
 
     def __init__(self, national_id, first_name, surname, dob, home_county, email, password):
         self.national_id = national_id
@@ -29,8 +36,41 @@ class User(db.Model):
         self.email = email
         self.password = password
 
+    def get_id(self):
+        return self.national_id
+
     def __repr__(self):
         return '<user_id {}>'.format(self.user_id)
+
+
+class Admin(db.Model):
+    """ Create user table"""
+
+    __tablename__ = 'admin'
+
+    national_id = db.Column(db.Integer, nullable=False, primary_key=True)
+    first_name = db.Column(db.String, nullable=False)
+    surname = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    created = db.Column(db.TIMESTAMP, nullable=False, default=datetime.utcnow)
+    last_seen = db.Column(db.TIMESTAMP, nullable=False, default=datetime.utcnow)
+    confirmed = db.Column(db.Boolean, nullable=False, default=False)
+    confirmed_on = db.Column(db.DateTime, nullable=True)
+    role = db.Column(db.String, nullable=False, default="Admin")
+
+    def __init__(self, national_id, first_name, surname, email, password):
+        self.national_id = national_id
+        self.first_name = first_name
+        self.surname = surname
+        self.email = email
+        self.password = password
+
+    def get_id(self):
+        return self.national_id
+
+    def __repr__(self):
+        return '<admin_id {}>'.format(self.admin_id)
 
 
 class County(db.Model):
