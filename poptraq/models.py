f3894cf5b5443065f7862192ebd2d7cdd2c9f4c1
Maxwell-Icharia <maxwellicharia@gmail.com>
2018-10-22 02:20:24 +0300

    [Edit] - Remove User table

--- poptraq/models.py
+++ poptraq/models.py
@@ -4,12 +4,12 @@ from datetime import datetime
 from flask_login import UserMixin
 
 from poptraq import db
-from poptraq import login
+from poptraq import login_manager
 
 
-@login.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+@login_manager.user_loader
+def load_user(login_id):
+    return User.query.get(int(login_id))
 
 
 class User(db.Model, UserMixin):
@@ -28,56 +28,20 @@ class User(db.Model, UserMixin):
     home_county = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
-    created = db.Column(db.String, nullable=False, default=d_aware.strftime("%B %d, %Y | %H:%M:%S"))
-    updated = db.Column(db.String, nullable=False, default=d_aware.strftime("%B %d, %Y | %H:%M:%S"))
-    last_seen = db.Column(db.String, nullable=False, default=d_aware.strftime("%B %d, %Y | %H:%M:%S"))
+    created = db.Column(db.String, nullable=False, default=d_aware.strftime("%B %d, %Y %H:%M:%S"))
+    updated = db.Column(db.String, nullable=False, default=d_aware.strftime("%B %d, %Y %H:%M:%S"))
+    last_seen = db.Column(db.String, nullable=False, default=d_aware.strftime("%B %d, %Y %H:%M:%S"))
     confirmed = db.Column(db.Boolean, nullable=True)
     confirmed_on = db.Column(db.String, nullable=True)
-    role = db.Column(db.String, nullable=False, default="User")
+    user = db.Column(db.Boolean, nullable=False, default=True)
+    admin = db.Column(db.Boolean, nullable=False, default=False)
+    status = db.Column(db.String, nullable=False, default='Active')
 
     def __init__(self, national_id, first_name, surname, dob, home_county, email, password):
         self.national_id = national_id
         self.first_name = first_name
         self.surname = surname
         self.dob = dob
-        self.home_county = home_county
-        self.email = email
-        self.password = password
-
-    def get_id(self):
-        return self.national_id
-
-    def __repr__(self):
-        return '<national_id {}>'.format(self.national_id)
-
-
-class Admin(db.Model):
-    """ Create user table"""
-
-    d = datetime.now()
-    timezone = pytz.timezone("Africa/Nairobi")
-    d_aware = timezone.localize(d)
-
-    __tablename__ = 'admin'
-
-    national_id = db.Column(db.Integer, nullable=False, primary_key=True)
-    first_name = db.Column(db.String, nullable=False)
-    surname = db.Column(db.String, nullable=False)
-    dob = db.Column(db.Date, nullable=True)
-    home_county = db.Column(db.String, nullable=False)
-    email = db.Column(db.String, nullable=False, unique=True)
-    password = db.Column(db.String, nullable=False)
-    created = db.Column(db.String, nullable=False, default=d_aware.strftime("%B %d, %Y | %H:%M:%S"))
-    updated = db.Column(db.String, nullable=False, default=d_aware.strftime("%B %d, %Y | %H:%M:%S"))
-    last_seen = db.Column(db.String, nullable=False, default=d_aware.strftime("%B %d, %Y | %H:%M:%S"))
-    confirmed = db.Column(db.Boolean, nullable=True)
-    confirmed_on = db.Column(db.String, nullable=True)
-    role = db.Column(db.String, nullable=False, default="Admin")
-
-    def __init__(self, national_id, first_name, surname, home_county, email, password):
-        self.national_id = national_id
-        self.first_name = first_name
-        self.surname = surname
         self.home_county = home_county
         self.email = email
         self.password = password
