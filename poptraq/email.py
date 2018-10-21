83e850a482a9b6737a856a48e8c81de9d9643d4f
Maxwell-Icharia <maxwellicharia@gmail.com>
2018-10-22 02:03:41 +0300

    [Edit] - Threadings email

--- poptraq/email.py
+++ poptraq/email.py
@@ -1,8 +1,16 @@
+from threading import Thread
 from flask_mail import Message
-
 from poptraq import app, mail
 
 
 def send_email(to, subject, template):
     msg = Message(subject, recipients=[to], html=template, sender=app.config['MAIL_DEFAULT_SENDER'])
-    mail.send(msg)
+    thr = Thread(target=send_async_email, args=[msg])
+    thr.start()
+    return thr
+
+
+def send_async_email(msg):
+    with app.app_context():
+        print('====> Sending Email - Async')
+        mail.send(msg)
